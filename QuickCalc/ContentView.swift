9fa5acfa529e4ae88976e4a85047aeed6c698857
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-07-04 04:58:48 +0900

    Comment out Preview

--- QuickCalc/ContentView.swift
+++ QuickCalc/ContentView.swift
@@ -178,8 +178,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 */
+/*
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
+*/
